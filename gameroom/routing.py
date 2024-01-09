db3cba800efbdb674837201485e99e499eacebf6
Andy <andrew.j.c.e@gmail.com>
2024-01-08 22:50:37 -0500

    Added limitation to number of sneaks per round, need to remove limit when game is not restricted

--- gameroom/routing.py
+++
@@ -1,7 +0,0 @@
-# routing.py
-from django.urls import path
-from .consumers import GameRoomConsumer
-
-websocket_urlpatterns = [
-    path("ws/gameroom/<int:game_id>/", GameRoomConsumer.as_asgi()),
-]
